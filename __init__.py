e69bae56a3fa817becea06873bc206a61320b671
Qurban <qurban_ali36@yahoo.com>
2015-03-09 15:14:38 +0500

    ui added to specify a LD file

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 import src._connect as con
 reload(con)
-connect = con.connect
+Window = con.LD
--- src/_connect.py
+++ src/_connect.py
@@ -6,45 +6,80 @@ Created on Jan 7, 2015
 import pymel.core as pc
 import appUsageApp
 import qtify_maya_window as qtfy
-from PyQt4.QtGui import QMessageBox
+from uiContainer import uic
+from PyQt4.QtGui import QMessageBox, QFileDialog
 import msgBox
+import os.path as osp
 
-def connect():
-    objects = pc.ls(sl=True)
-    if not objects:
-        pc.warning('No selection found in the scene')
-        return
+root_path = osp.dirname(osp.dirname(__file__))
+ui_path = osp.join(root_path, 'ui')
+
+Form, Base = uic.loadUiType(osp.join(ui_path, 'main.ui'))
+class LD(Form, Base):
+    def __init__(self, parent=qtfy.getMayaWindow()):
+        super(LD, self).__init__(parent)
+        self.setupUi(self)
+        
+        self.addButton.clicked.connect(self.add)
+        self.browseButton.clicked.connect(self.setPath)
+        
+        appUsageApp.updateDatabase('AddLD')
+        
+    def setPath(self):
+        filename = QFileDialog.getOpenFileName(self, 'Select File',
+                                               '', '*.ma *.mb')
+        if filename:
+            self.pathBox.setText(filename)
+        
+    def setStatus(self, msg):
+        self.statusBar().showMessage(msg, 2000)
+
+    def add(self):
+        
+        objects = pc.ls(sl=True)
+        if not objects:
+            self.setStatus('No selection found in the scene')
+            return
+        
+        if len(objects) > 1:
+            self.setStatus('Select 1 object only (Maya object set)')
+            return
+        
+        _set = objects[0]
+        
+        if not pc.hasAttr(_set, 'forCache'):
+            pc.addAttr(_set, shortName='forCache', longName='forCache', niceName='LD', dt='string')
+            
+        # set the attribute
+        path = self.getPath()
+        if not path:
+            return
+        try:
+            _set.forCache.set(path)
+        except Exception as ex:
+            msgBox.showMessage(self, title='Error', msg=str(ex),
+                               icon=QMessageBox.Information)
+            return
+        
+        pc.inViewMessage(amg='<hl>LD added successfully</hl>', pos='midCenter', fade=True )
+        
+        self.close()
     
-    if len(objects) != 2:
-        pc.warning('Select 2 objects only (One mesh and one set)')
-        return
+    def getPath(self):
+        path = self.pathBox.text()
+        if not path:
+            self.setStatus('Path not specified')
+            return ''
+        if not osp.exists(path):
+            self.setStatus('Path does not exist')
+            return ''
+        if not osp.isfile(path):
+            self.setStatus('Specified path is not a file')
+            return ''
+        if osp.splitext(path)[-1] not in ['.ma', '.mb']:
+            self.setStatus('Specified file is not a Maya file')
+            return ''
+        return path
     
-    try:
-        _set = pc.ls(objects, type=pc.nt.ObjectSet)[0]
-    except IndexError:
-        pc.warning('Set not found in the selection')
-        return
-    
-    for obj in objects:
-        if not pc.hasAttr(obj, 'forCache'):
-            pc.addAttr(obj, shortName='forCache', longName='forCache', at='message')
-    
-    objects.remove(_set)
-    mesh = objects[0]
-    
-    if pc.polyEvaluate(_set, f=True) != pc.polyEvaluate(mesh, f=True):
-        msgBox.showMessage(qtfy.getMayaWindow(), title='Compatibility Error',
-                           msg='Selected objects are not compatible',
-                           icon=QMessageBox.Information)
-        return
-    
-    try:
-        _set.forCache.disconnect()
-        _set.forCache.connect(mesh.forCache)
-    except Exception as ex:
-        pc.confirmDialog(title='Error', message=str(ex), button='Ok')
-        return
-    
-    pc.inViewMessage(amg='<hl>Objects connected successfully</hl>', pos='midCenter', fade=True )
-    
-    appUsageApp.updateDatabase('ObjectsConnect')
+    def closeEvent(self, event):
+        self.deleteLater()
